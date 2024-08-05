7f18e29dd04515bc4f4afd58e94d16571992006b
Denise <deniselewis1120@gmail.com>
2024-08-05 10:42:17 -0700

    Day 1: Data Types

--- python/day1.py
+++ python/day1.py
@@ -1,7 +1,15 @@
-# Read a full line of input from stdin and save it to our dynamically typed variable, input_string.
-input_string = input()
-
-# Print a string literal saying "Hello, World." to stdout.
-print('Hello, World.')
-
-# TODO: Write a line of code here that prints the contents of input_string to stdout.
+i = 4
+d = 4.0
+s = 'HackerRank '
+# Declare second integer, double, and String variables.
+# Read and save an integer, double, and String to your variables.
+i2 = int(input())
+d2 = float(input())
+s2 = input()
+# Print the sum of both integer variables on a new line.
+print(i + i2)
+# Print the sum of the double variables on a new line.
+print(d + d2)
+# Concatenate and print the String variables on a new line
+# The 's' variable above should be printed first.
+print(s + s2)
