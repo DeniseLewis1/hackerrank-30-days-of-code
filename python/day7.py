c4930aed03a62a5cd06f93437ce7743eb904f89c
Denise <deniselewis1120@gmail.com>
2024-08-14 09:31:16 -0700

    Day 7: Arrays

---
+++ python/day7.py
@@ -0,0 +1,23 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+
+
+if __name__ == '__main__':
+    n = int(input().strip())
+
+    arr = list(map(int, input().rstrip().split()))
+    
+    reversed_arr = arr[::-1]
+    
+    output = ""
+    
+    for i in reversed_arr:
+        output += f"{i} "
+        
+    print(output)
